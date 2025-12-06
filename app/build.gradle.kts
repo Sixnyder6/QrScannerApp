7c82a3cdc1a0a482630cb6f192885d6652a583f9
Sixnyder6 <79064560720@mail.ru>
2025-12-06 07:43:03 +0300

    Bump version to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.qrscannerapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.3.2"
+        versionCode = 26
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
